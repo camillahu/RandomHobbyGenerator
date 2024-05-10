51dd12cd2e591a551c1f82c4141430e18b3e63f3
camillahu <camillahaugenurdal@gmail.com>
2024-05-10 13:06:46 +0200

    Add project files.

---
+++ RandomHobbyGenerator/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System.IO.Enumeration;
+
+Random rand = new Random();
+var name = "";
+var exitOrNew = "";
+
+
+void Run()
+{   
+    while (true) {
+        Console.WriteLine("Who would Like a new hobby?");
+        name = Console.ReadLine();
+        GetHobby(); 
+        Console.WriteLine("Type 'exit' to exit or type anything else for a new hobby");
+        exitOrNew = Console.ReadLine();
+        GoodBye();
+    }
+}
+
+
+void GetHobby()
+{
+    var randHobby = rand.Next(0, 5);
+    if (randHobby == 0)
+    {
+        Console.WriteLine($"{name} is now a magician!");
+    }
+    else if(randHobby == 1)
+    {
+        Console.WriteLine($"{name} is now a professional toad-licker!");
+    }
+    else if (randHobby == 2)
+    {
+        Console.WriteLine($"{name} is now a singer!");
+    }
+    else if (randHobby == 3)
+    {
+        Console.WriteLine($"{name} is now a professional fish-cuddler!");
+    }
+    else 
+    {
+        Console.WriteLine($"{name} is now a professional Waldo-finder!");
+    }
+}
+
+void GoodBye()
+{
+    if (exitOrNew == "exit")
+    {
+        Environment.Exit(0);
+    }
+}
+
+Run();
